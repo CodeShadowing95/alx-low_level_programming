cad9fa6937576aa66c81e9173c26edc41739028f
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-10 20:04:25 -0800

    New Changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %2ld byte(s)\n", (unsigned long)sizeof(char));
-printf("Size of an int: %2ld byte(s)\n", (unsigned long)sizeof(int));
-printf("Size of a long int: %2ld byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %2ld byte(s)\n", (unsigned long)sizeof(long long int));
-printf("Size of a float: %2ld byte(s)\n", (unsigned long)sizeof(float));
+printf("Size of a char: %2ld byte(s)", (unsigned long)sizeof(char));
+printf("Size of an int: %2ld byte(s)", (unsigned long)sizeof(int));
+printf("Size of a long int: %2ld byte(s)", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %2ld byte(s)", (unsigned long)sizeof(long long int));
+printf("Size of a float: %2ld byte(s)", (unsigned long)sizeof(float));
 
 return (0);
 }
