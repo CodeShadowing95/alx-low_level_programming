85b01cd221e8c71e28c3c3ae274fb990ffcfd050
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-10 18:58:06 -0800

    New Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle"); return 0;
+/* /bin/bash */
+# include <stdio.h>
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -7,5 +7,5 @@
  */
 int main(void)
 {
-puts("\"Cool");
+puts("\"Cool\n");
 }
