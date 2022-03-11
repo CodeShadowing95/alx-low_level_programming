3c56ac551a366681316fc8721ba0659a6121a59d
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-10 19:21:25 -0800

    New Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-/* /bin/bash */
 # include <stdio.h>
 
-int main(void)
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+void main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
