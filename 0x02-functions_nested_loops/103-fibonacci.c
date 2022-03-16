29234a8badbcba09e527de5d55bee853e28cd135
CodeShadowing95 <49642047+CodeShadowing95@users.noreply.github.com>
2022-03-16 21:12:46 +0100

    Fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,21 +6,21 @@
  * Return: Always 0
  */
 int main(void) {
-    int n1 = 0, n2 = 1, n3, sum = 0;
+    unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
     int count = 0;
 
-    while (count <= 10) {
+    while (count <= 31) {
         n3 = n1 + n2;
         if (n3 % 2 == 0)
         {
             sum += n3;
         }
         n1 = n2;
-        n2 = sum;
+        n2 = n3;
         count++;
     }
 
-    printf("%d\n", sum);
+    printf("%ld\n", sum);
 
     return (0);
 }
