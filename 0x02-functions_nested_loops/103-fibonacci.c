e14f81b5426ea34d7e32bfcf0bc249f402d56213
CodeShadowing95 <49642047+CodeShadowing95@users.noreply.github.com>
2022-03-16 21:26:21 +0100

    Changes

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,25 +5,23 @@
  *
  * Return: Always 0
  */
-int main(void)
-{
-unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
-int count = 0;
+int main(void) {
+    unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+    int count = 0;
 
-while (count <= 31)
-{
-n3 = n1 + n2;
-if (n3 % 2 == 0)
-{
-sum += n3;
-}
-n1 = n2;
-n2 = n3;
-count++;
-}
+    while (count < 31) {
+        n3 = n1 + n2;
+        if (n3 % 2 == 0)
+        {
+            sum += n3;
+        }
+        n1 = n2;
+        n2 = n3;
+        count++;
+    }
 
-printf("%ld\n", sum);
+    printf("%ld\n", sum);
 
-return (0);
+    return (0);
 }
 
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long n1 = 0, n2 = 1, sum;
+int count = 0;
+
+while (count < 98)
+{
+sum = n1 + n2;
+n1 = n2;
+n2 = sum;
+if (count == 97)
+{
+printf("%ld", sum);
+} else {
+printf("%ld, ", sum);
+}
+count++;
+}
+
+printf("\n");
+
+return (0);
+}
+
