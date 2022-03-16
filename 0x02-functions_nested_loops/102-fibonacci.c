5899bad433d3c3369ff6a3b1e115a09a3fe0a3d8
CodeShadowing95 <49642047+CodeShadowing95@users.noreply.github.com>
2022-03-16 19:40:49 +0100

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,29 +5,24 @@
  *
  * Return: Always 0
  */
-int main(void)
-{
-unsigned long n1 = 0, n2 = 1, sum;
-int count = 0;
+int main(void) {
+    unsigned long n1 = 0, n2 = 1, sum;
+    int count = 0;
 
-while (count < 50)
-{
-sum = n1 + n2;
-n1 = n2;
-n2 = sum;
-if (count == 49)
-{
-printf("%ld", sum);
-}
-else
-{
-printf("%ld, ", sum);
-}
-count++;
-}
+    while (count < 50) {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+        if (count == 49) {
+            printf("%ld", sum);
+        } else {
+            printf("%ld, ", sum);
+        }
+            count++;
+    }
 
-printf("\n");
+    printf("\n");
 
-return (0);
+    return (0);
 }
 
