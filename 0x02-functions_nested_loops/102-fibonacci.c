9e4d43b87c28ba0f5fa5e1f1333c3d014e38bc3f
CodeShadowing95 <49642047+CodeShadowing95@users.noreply.github.com>
2022-03-16 19:36:04 +0100

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 unsigned long n1 = 0, n2 = 1, sum;
 int count = 0;
 
-while (count <= 5)
+while (count <= 50)
 {
 sum = n1 + n2;
 n1 = n2;
 n2 = sum;
-if (count == 5)
+if (count == 50)
 {
 printf("%ld", sum);
 }
