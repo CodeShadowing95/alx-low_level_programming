510c66804573a1a11e7400c3c9018c83bf492c9b
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-16 10:52:07 -0700

    Task 14 - Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-int n1 = 0, n2 = 1, sum, count = 0;
+unsigned long long n1 = 0, n2 = 1, sum;
+int count = 0;
 
 while (count <= 50)
 {
 sum = n1 + n2;
 n1 = n2;
 n2 = sum;
-printf("%d, ", sum);
+printf("%lld, ", sum);
 count++;
 }
 
