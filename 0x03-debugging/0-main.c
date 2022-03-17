17ee34da128cd36d58f22ca953f999f6d5e49dd7
CodeShadowing95 <49642047+CodeShadowing95@users.noreply.github.com>
2022-03-17 12:20:54 +0100

    Task 0 - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * 
+ * @return Always 0
+ */
+int main(void)
+{
+    int i;
+    positive_or_negative(i);
+
+    return (0);
+}
