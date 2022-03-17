da995610a0d9b13dc9419b93fe0fce10e0aefed6
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-17 12:19:42 -0700

    Task4.0 - isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isupper - Entry point
+ * @c: parameter
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0- Check for uppercase character
