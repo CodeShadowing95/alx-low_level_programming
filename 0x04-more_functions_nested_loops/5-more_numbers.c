39c17259f88b633649774854e92b7542f7d11f9d
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-18 11:35:40 -0700

    Task4.5 - More numbers 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,27 +2,25 @@
 
 /**
  * more_numbers - Entry point
- *
- * Return: 10 times the numbers from 0 to 14
+ * Return: 10 times numbers from 0 to 14
  */
 void more_numbers(void)
 {
-int i = 0;
-int j;
+int i = 0, j;
+
 while (i < 10)
 {
 j = 0;
 while (j < 15)
 {
-_putchar(j + '0');
-if (j >= 10)
+if (j / 10 == 1)
 {
-_putchar((j % 10) + '0');
+_putchar((j / 10) + '0');
 }
+_putchar((j % 10) + '0');
 j++;
 }
 _putchar('\n');
 i++;
 }
-
 }
