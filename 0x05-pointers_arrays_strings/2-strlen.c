d41f6443002dc52f036d67787b6d6c08e930c48c
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-21 13:19:54 -0700

    Task5.2 - Lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Entry point
+ * @s: pointer to char
+ * Return: lenght of a string
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0- Function that takes a pointer to an int as parameter and updates the value it points to to 98.</br>
 1- Function that swaps the values of two integers</br>
+2- Lenght of a string</br>
 
