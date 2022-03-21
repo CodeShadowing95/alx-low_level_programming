71dffca415164cbc7da49f9d7e19bbee00f373eb
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-21 16:37:09 -0700

    Task5.8 - Array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,20 @@
 void print_array(int *a, int n)
 {
 int i = 0;
-
+if (n > 0)
+{
 while (i < n)
 {
 if (i == n - 1)
 {
-printf("%d\n", a[i]);
+printf("%d", a[i]);
 }
 else
 {
 printf("%d, ", a[i]);
 }
 i++;
+}
+printf("\n");
 }
 }
