b3dba5ac7ea016a3eb90fcf85e77b8610a347a4d
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-22 03:04:54 -0700

    Task5.9 - strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - Entry point
+ *
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * Return: String
+ */
+char *_strcpy(char *dest, char *src)
+{
+return strcpy(dest, src);
+}
--- 0x05-pointers_arrays_strings/test_main.c
+++ 0x05-pointers_arrays_strings/test_main.c
@@ -8,9 +8,11 @@
  */
 int main(void)
 {
-char *str;
+char s1[98];
+char *ptr;
 
-str = "0123456789";
-puts_half(str);
+ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+printf("%s", s1);
+printf("%s", ptr);
 return (0);
 }
