4a73f3f9a4d46aafc8b8a06917894113d819fd23
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-23 08:20:54 -0700

    Task6.0 - strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - Concatenates 2 strings
+ * @dest: destination string
+ * @src: source string
+ * Return: String concatenated
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0- Concatenates 2 strings
+
