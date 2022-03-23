8a105dc5e50d0c4fcb9c84d78c0dfdb27bf2afe0
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-23 09:32:44 -0700

    Task6.3 - strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - Compare 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: value > 0 if s1 < s2, value < 0 if s1 > s2, 0 if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 0- Concatenate 2 strings with strcat</br>
 1- Concatenate 2 strings with strncat</br>
 2- Copy a string</br>
+3- Compare 2 strings</br>
 
