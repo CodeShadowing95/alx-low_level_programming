9c5ad31d7e19746e3add9cf6f3c73cb3910757b2
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-24 13:35:02 -0700

    Task6.101 - Print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 {
 if (n < 0)
 {
-_putchar(45);
+_putchar('-');
 n = -n;
 }
 
@@ -19,5 +19,5 @@ if (n / 10)
 {
 print_number(n / 10);
 }
-_putchar((n % 10) + '0');
+_putchar(n % 10 + '0');
 }
