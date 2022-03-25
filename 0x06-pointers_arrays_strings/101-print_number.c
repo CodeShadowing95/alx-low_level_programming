7130a0af8abf8f2ab3e3b7ed765b3ce2faab891c
CodeShadowing95 <49642047+CodeShadowing95@users.noreply.github.com>
2022-03-25 13:30:10 +0100

    Task6.101 - print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,15 +9,22 @@
  */
 void print_number(int n)
 {
+
+unsigned int nbr;
+
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+nbr = -n;
+}
+else
+{
+nbr = n;
 }
 
-if (n / 10)
+if (nbr / 10)
 {
-print_number(n / 10);
+print_number(nbr / 10);
 }
-_putchar(n % 10 + '0');
+_putchar((nbr % 10) + '0');
 }
