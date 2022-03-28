8d05cffc10ee296b294a7a33a9a8441a022662d0
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-28 05:39:45 -0700

    Task7.1 - Memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * memcpy - Copy n bytes from memory area src to memory area dest
+ *
+ * @dest: memory area destination
+ * @src: memory area source
+ * @n: number of bytes to copy
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0- Function that fills memory with a constant byte</br>
+1- Function that copies memory area</br>
 
