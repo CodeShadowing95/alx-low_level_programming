ec8f7ddad0d60303267a6d3de4093635cd8c521d
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-28 06:52:44 -0700

    Task7.3 - strspn function

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,6 @@
  * @accept: string containing the list of chars to match in s
  * Return: number of chars in the initial segment of s which consist only
  * of chars from str2
-:q
  */
 unsigned int _strspn(char *s, char *accept)
 {
