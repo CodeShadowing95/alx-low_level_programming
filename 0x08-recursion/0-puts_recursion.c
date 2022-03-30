0f669627537eaca4f1bddebf0b71e027c25719c0
Frank Patrick <ksenbonsakura@gmail.com>
2022-03-30 12:24:26 -0700

    Task8.0 - Rec. String

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,14 +12,9 @@ void _puts_recursion(char *s)
 	/** while there's a character in the string **/
 	if (*s)
 	{
-		/** Recursion to the string, get the next character**/
-		_puts_recursion(s - 1);
 		/** print the character **/
 		puts(s);
-	}
-
-	if (!(*s))
-	{
-		puts("\n");
+		/** Recursion to the string, get the next character**/
+		_puts_recursion(s - 1);
 	}
 }
