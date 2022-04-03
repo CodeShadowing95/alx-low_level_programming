60d5f0f2f30ac92fccaaba28db149f6c33e78b3d
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-02 23:01:37 -0700

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  * @argv: vector of command-lines arguments
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
