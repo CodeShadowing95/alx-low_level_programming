56b770e1f9aef0896995bb369f835189a2004439
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-02 23:22:50 -0700

    print number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  * @argv: vector of arguments
  * Return: Always 0.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int i = 0;
 	int count = 0;
