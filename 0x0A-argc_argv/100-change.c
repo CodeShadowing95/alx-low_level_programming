1222f3c7a429847e3c2bd1687fe1998eb6db86f6
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-04 17:20:38 -0700

    coins changes

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - make change of an amount of money
+ *
+ * @argc: arguments count
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int change = 0, nbr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	nbr = atoi(argv[1]);
+
+	if (nbr < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	if (nbr % 25 >= 0)
+	{
+		change += nbr / 25;
+		nbr %= 25;
+	}
+	if (nbr % 10 >= 0)
+	{
+		change += nbr / 10;
+		nbr %= 10;
+	}
+	if (nbr % 5 >= 0)
+	{
+		change += nbr / 5;
+		nbr %= 5;
+	}
+	if (nbr % 2 >= 0)
+	{
+		change += nbr / 2;
+		nbr %= 2;
+	}
+	if (nbr % 1 >= 0)
+	{
+		change += nbr / 1;
+	}
+
+	printf("%d\n", change);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -3,3 +3,4 @@
 2- Print all arguments a program recieves</br>
 3- Print the multiplies two numbers</br>
 4- Add positive numbers</br>
+5- Minimum number of coins to make change for an amount of money
