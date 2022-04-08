62e62caad00e5b902cae5591e11f0d6d55d17b8a
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-07 19:54:45 -0700

    malloc - _calloc

--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,3 @@
 0- Function that allocates memory using malloc</br>
 1- Concatenate 2 strings</br>
+2- Allocate memory for an array, using malloc</br>
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,4 +2,6 @@ void *malloc_checked(unsigned int);
 
 char *string_nconcat(char *, char *, unsigned int);
 
+void *_calloc(unsigned int, unsigned int);
+
 
