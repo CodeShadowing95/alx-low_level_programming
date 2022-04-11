27adbd978f6a164fc544c05ac613f03b0427df49
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-11 05:26:53 -0700

    init struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
-		return;
+		exit(98);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	free(d);
 }
 
