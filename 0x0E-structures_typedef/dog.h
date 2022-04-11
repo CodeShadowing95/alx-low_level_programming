da52b1bdce743eae3481356af7a74f6d2c513de8
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-11 04:36:07 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG_H
 /**
  * struct dog - new type dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ *
+ * Description: Create the structure dog
  */
 struct dog {
 	char *name;
