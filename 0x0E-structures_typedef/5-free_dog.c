42afb14660adccd5ab9dd20c9aa4cfde6c3e3a96
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-13 09:13:48 -0700

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,11 +9,11 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d)
 	{
-		if (d->name != NULL)
+		if (d->name)
 			free(d->name);
-		if(d->owner != NULL)
+		if(d->owner)
 			free(d->owner);
 		free(d);
 	}
