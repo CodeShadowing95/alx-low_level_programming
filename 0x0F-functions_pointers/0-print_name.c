5c3743d94d550fbe478443a43a0d1ce06575b33f
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-14 03:41:33 -0700

    print name

---
+++ 0x0F-functions_pointers/0-print_name.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * printStr - function
+ *
+ * @str: string
+ * Return: void
+ */
+void printStr(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+	i += 1;
+
+	for (int j = 0; str[j] != 0; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
+
+/**
+ * print_name - display a string using function pointer
+ *
+ * @name: string to print
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
