df4a9bd2e6ba7449eb49a8081d4648807b1a1b27
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-14 03:43:27 -0700

    print a name

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+0x0F: Functions pointers
+
+Function that prints a name</br>
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+int _putchar(char);
+
+void printStr(char *);
+
+void print_name(char *, void (*)(char *));
+
+#endif
