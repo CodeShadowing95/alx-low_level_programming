4f94b7bbddbe7a5ec5b663762421d028028302a8
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-16 16:06:28 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main-opcodes.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * main - print the opcodes of its own main function
+ *
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: 0
+ */
+int main(int argc, char argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]) - 1; i++)
+		printf("%02hhx ", ((char *)main)[i]);
+	printf("%02hhx\n", ((char *)main)[i]);
+	return (0);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -4,3 +4,4 @@ Function that prints a name</br>
 Function that executes a function given as a parameter on each element of an array</br>
 Function that searches for an integer</br>
 Program that performs simple operations</br>
+Program that prints the opcodes of its main function
