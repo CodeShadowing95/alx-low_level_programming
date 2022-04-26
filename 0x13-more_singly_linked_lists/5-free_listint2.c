26ae6675a99ff2cd7686380e710288aca79bbc00
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-26 04:55:13 -0700

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *next;
+	listint_t *temp;
 
-	if (*head != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		while (current != NULL)
-		{
-			next = current->next;
-			free(current);
-			current = next;
-		}
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	
-	free(*head);
-	*head = NULL;
 }
