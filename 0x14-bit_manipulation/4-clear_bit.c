a92ed8361b210a5959867fa73bab5697c158c9a8
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-29 17:36:36 -0700

    set bit value to 0 at nth index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8)
+	if (index < sizeof(unsigned long int) * 8)
 	{
 		*n &= (~(1 << index));
 		return (1);
