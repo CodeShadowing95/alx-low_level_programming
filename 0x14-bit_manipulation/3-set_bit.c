6a0111a7bbcfcfb09c79341f9afa3700631db528
Frank Patrick <ksenbonsakura@gmail.com>
2022-04-29 17:04:33 -0700

    set bit value to 1 at nth index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,5 +11,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
-	return ((*n |= 1 << index) ? 1 : - 1);
+	return ((*n |= 1 << index) ? 1 : -1);
 }
