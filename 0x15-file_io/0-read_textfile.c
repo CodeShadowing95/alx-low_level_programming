230937353ba92276b9145e7b5144e05ae8b1aecd
Frank Patrick <ksenbonsakura@gmail.com>
2022-05-03 10:32:44 -0700

    read text file and print it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+#include <unistd.h>
+
+#include <fcntl.h>
+
+/**
+ * read_textfile - read a text file and prints it to stdout
+ *
+ * @filename: ...
+ * @letters: number of letters it should read and print
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wd;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	/* open the text file */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+		return (0);
+
+	/* read the text file */
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+		return (0);
+	buf[letters] = '\0';
+
+	/* write to the standard output 1 or STDOUT_FILENO */
+	wd = write(STDOUT_FILENO, buf, letters);
+	if (wd == -1)
+		return (0);
+
+	close(fd);
+	free(buf);
+
+	return (letters);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+0x15 - FIle IO
+
+Function that reads a text file and prints it to the POSIX standard output</br>
