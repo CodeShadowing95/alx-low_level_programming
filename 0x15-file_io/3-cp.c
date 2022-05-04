5b9b920650813d6d3973e60c33b303612588bda2
Frank Patrick <ksenbonsakura@gmail.com>
2022-05-04 02:54:01 -0700

    copy file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void cpFile(const char *, const char *);
+
+/**
+ * main - check the code
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cpFile(argv[1], argv[2]);
+	return (0);
+}
+
+/**
+ * cpFile - copy the content of a file to another file
+ *
+ * @file_from: filename of the source file or file to copy
+ * @file_to: filename of the destination file
+ * Return: void
+ */
+void cpFile(const char *file_from, const char *file_to)
+{
+	int src, dest, rd;
+	char buf[1024];
+
+	src = open(file_from, O_RDONLY);
+	if (src == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	dest = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	/* if we got the informations from source file successfully */
+	while ((rd = read(src, buf, 1024)) > 0)
+	{
+	/* if the nbytes that were written in destination file */
+	/* != to nbytes that were read in source file, error */
+		if (write(dest, buf, rd) != rd || dest == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+
+	if (close(src) == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", src);
+		exit(100);
+	}
+
+	if (close(dest) == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", dest);
+		exit(100);
+	}
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -3,3 +3,4 @@
 Function that reads a text file and prints it to the POSIX standard output</br>
 Function that creates a file</br>
 Function that appends text at the end of a file</br>
+Function that copies the content of a file to another file</br>
